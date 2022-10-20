ec8775ee48659177909cb6079b472e06797480dd
maito1201 <itomasa0915@gmail.com>
2022-10-20 23:53:32 +0900

    chore: install prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import { useMetaMask } from "metamask-react";
+import { useMetaMask } from 'metamask-react'
 
 import './App.css'
 
 function App() {
   const { status, connect, account, chainId, ethereum } = useMetaMask()
 
-  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+  if (status === 'initializing') return <div>Synchronisation with MetaMask ongoing...</div>
 
-  if (status === "unavailable") return <div>MetaMask not available :(</div>
+  if (status === 'unavailable') return <div>MetaMask not available :(</div>
 
-  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+  if (status === 'notConnected') return <button onClick={connect}>Connect to MetaMask</button>
 
-  if (status === "connecting") return <div>Connecting...</div>
+  if (status === 'connecting') return <div>Connecting...</div>
 
   console.log(ethereum)
 
   return (
     <>
-      <div>Connected account {account} on chain ID {chainId}</div>
+      <div>
+        Connected account {account} on chain ID {chainId}
+      </div>
     </>
   )
 }
