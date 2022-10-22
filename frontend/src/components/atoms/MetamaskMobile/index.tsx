f202ad9632d840cd6a71b0d0e85d7ae7136037cd
maito1201 <itomasa0915@gmail.com>
2022-10-22 16:41:21 +0900

    fix target

--- frontend/src/components/atoms/MetamaskMobile/index.tsx
+++ frontend/src/components/atoms/MetamaskMobile/index.tsx
@@ -3,8 +3,8 @@ const metamaskAppDeepLink = 'https://metamask.app.link/dapp/' + dappUrl
 
 export const MetamaskMobile = () => {
   return (
-    <a href={metamaskAppDeepLink}>
-      <button>Connect to MetaMask</button>
+    <a href={metamaskAppDeepLink} target="_self">
+      <button>test, Connect to MetaMask</button>
     </a>
   )
 }
