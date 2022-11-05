a3e172f83e660fc6b323e6975c26d205b640498d
yokinist <ikd618@gmail.com>
2022-11-05 10:45:52 +0900

    fix: build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,14 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { useWeb3Client } from './modules/hooks/web3client'
 
-import { Stream, Collection, StreamSetting, Login } from './components/pages'
-import { createBrowserRouter } from 'react-router-dom'
+import { Login } from './components/pages'
+import { router } from './modules/router'
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ethereum?: any
   }
 }
-
-const router = createBrowserRouter([
-  { path: '/', element: <Stream /> },
-  { path: 'collection', element: <Collection /> },
-  { path: 'stream-setting', element: <StreamSetting /> },
-  { path: '*', element: <p>404 - Not Found</p> }
-])
 
 function App() {
   const { account, setAccount } = useWeb3Client()
--- frontend/src/modules/router/index.tsx
+++ frontend/src/modules/router/index.tsx
@@ -1,10 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Stream, Collection, StreamSetting, Login } from '../../components/pages'
+import { Stream, Collection, StreamSetting } from '../../components/pages'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Stream /> },
   { path: 'collection', element: <Collection /> },
   { path: 'stream-setting', element: <StreamSetting /> },
-  { path: 'login', element: <Login /> },
   { path: '*', element: <p>404</p> }
 ])
