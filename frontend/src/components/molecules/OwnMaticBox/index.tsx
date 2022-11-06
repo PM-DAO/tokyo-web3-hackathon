005a60405a61aa483e9e69ae76a49379689406d0
yokinist <ikd618@gmail.com>
2022-11-06 14:32:50 +0900

    style: tweak header layout

--- frontend/src/components/molecules/OwnMaticBox/index.tsx
+++ frontend/src/components/molecules/OwnMaticBox/index.tsx
@@ -9,14 +9,14 @@ type Props = {
 export const OwnMaticBox = ({ ownMaticAmount }: Props) => {
   const { deepBGColor, contrastTextColor } = useThemeColor()
   return (
-    <Container bgColor={deepBGColor} w="max-content" borderRadius="sm" py="2">
-      <Text fontSize="9px" color={contrastTextColor} fontWeight="bold">
+    <Container bgColor={deepBGColor} w="max-content" borderRadius="md" pt="3" pb="2">
+      <Text fontSize="9px" color={contrastTextColor} fontWeight="bold" fontFamily="poppins">
         現在保有しているトークン
       </Text>
       <Box textAlign="right">
         <Text display="inline" fontSize="14px" fontFamily="poppins" fontWeight="bold" color={contrastTextColor} textAlign="right">
           {parseFloat(ownMaticAmount).toFixed(6)}
-        </Text>
+        </Text>{' '}
         <Text display="inline" fontSize="2xs" fontFamily="poppins" color={contrastTextColor} textAlign="right">
           MATIC
         </Text>
