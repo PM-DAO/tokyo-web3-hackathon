b641ea04db54245a61ccb99f4423486b55e002ca
yokinist <ikd618@gmail.com>
2022-11-06 11:02:02 +0900

    fix: formatTimeByAttributes for end time exceeds 60 minutes

--- frontend/src/modules/token/index.ts
+++ frontend/src/modules/token/index.ts
@@ -9,6 +9,18 @@ export const formatTimeByAttributes = (attributes: TokenType['metadata']['attrib
 
   if (!hour || !minutes) return null
 
+  const numberMinutes = Number(minutes)
+
+  // #NOTE: 終了時刻が60分を超える時
+  if (numberMinutes + STREAMING_DURATION_MINUTES >= 60) {
+    const endHour = Number(hour) + 1
+    const endMinutes = numberMinutes + STREAMING_DURATION_MINUTES - 60
+    return {
+      startTime: `${hour}:${minutes}`,
+      endTime: `${endHour}:${endMinutes}`
+    }
+  }
+
   const endMinutes = Number(minutes) + STREAMING_DURATION_MINUTES
 
   return {
